fe4a58420261ddaa1b8c7f90cb7b3d9a082a72a6
7dolphins <7dolphins@gmail.com>
2023-10-26 13:45:16 +0900

    until callback

---
+++ syntax/function2.js
@@ -0,0 +1,9 @@
+console.log(Math.round(1.6));   //round는 반올림
+console.log(Math.round(1.4));  
+
+sum(2,4);
+
+function sum(first,second){
+    let total = first + second;
+    return console.log(total);
+}
